ef8b4c057dd5042f61b4755871b0d58b1e2cc894
Hitesh Sabnani <hiteshsabnani3@gmail.com>
2018-09-26 09:08:02 -0700

    added code for SGD as well as effect of high learning rate

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,13 +4,15 @@ import time
 
 class LinearRegression(BaseEstimator):
 
-    def __init__(self, learning_rate=0.001, reg_strength=0, regularization="Ridge", max_iter=1000, gd_threshold=0.01, iteration_threshold=100):
+    def __init__(self, learning_rate=0.001, reg_strength=0, regularization="Ridge", max_iter=1000, cost_threshold=None,
+                 iteration_threshold=100, method="GD"):
         self.learning_rate = learning_rate
         self.reg_strength = reg_strength
         self.regularization = regularization
-        self.gd_threshold = gd_threshold
+        self.cost_threshold = cost_threshold
         self.max_iter = max_iter
         self.iteration_threshold = iteration_threshold
+        self.method = method
 
     def fit(self, X, y):
         """
@@ -32,21 +34,43 @@ class LinearRegression(BaseEstimator):
         iter = 0
         cost = curr_cost.copy()
         iterations = np.array(iter)
-        while self.gd_threshold is None or (prev_cost - curr_cost) > self.gd_threshold:
-            #print(iter, theta, curr_cost)
-            prev_cost = curr_cost
-            theta = theta - (1/num_examples) * self.learning_rate * (sum((h - y)*X) + self.reg_strength*theta)
-            bias = bias - (1/num_examples) * self.learning_rate * sum((h - y))
-            h = bias + np.dot(X,theta.T)
-            curr_cost = np.sqrt((1/(2*num_examples)) * sum((h - y)**2))
+        if self.method == "GD":
+            while self.cost_threshold is None or (prev_cost - curr_cost) > self.cost_threshold:
+                prev_cost = curr_cost
+                theta = theta - (1/num_examples) * self.learning_rate * (sum((h - y)*X) + self.reg_strength*theta)
+                bias = bias - (1/num_examples) * self.learning_rate * sum((h - y))
+                h = bias + np.dot(X,theta.T)
+                curr_cost = np.sqrt((1/(2*num_examples)) * sum((h - y)**2))
 
-            iter += 1
-            if iter % self.iteration_threshold == 0:
-                cost = np.append(cost, [curr_cost])
-                iterations = np.append(iterations, [iter])
+                iter += 1
+                if iter % self.iteration_threshold == 0:
+                    cost = np.append(cost, [curr_cost])
+                    iterations = np.append(iterations, [iter])
 
-            if iter >= self.max_iter:
-                break
+                if iter >= self.max_iter:
+                    break
+
+        elif self.method == "SGD":
+            while self.cost_threshold is None or (prev_cost - curr_cost) > self.cost_threshold:
+                # shuffle at each epoch
+                data_idx = [i for i in range(0,num_examples)]
+                np.random.shuffle(data_idx)
+                X = X[data_idx,]
+                y = y[data_idx,]
+                prev_cost = curr_cost
+                for i in range(0,num_examples):
+                    theta = theta - self.learning_rate * ((h - y)*X)[i,] - (1/num_examples) * self.learning_rate * self.reg_strength * theta
+                    bias = bias - self.learning_rate * (h - y)[i,]
+                    h = bias + np.dot(X,theta.T)
+                curr_cost = np.sqrt((1/(2*num_examples)) * sum((h - y)**2))
+
+                iter += 1
+                if iter % self.iteration_threshold == 0:
+                    cost = np.append(cost, [curr_cost])
+                    iterations = np.append(iterations, [iter])
+
+                if iter >= self.max_iter:
+                    break
 
 
         end_time = time.time()
@@ -63,3 +87,4 @@ class LinearRegression(BaseEstimator):
     def predict(self, X):
         y = self._intercept + np.dot(X, self._coef.T)
         return y
+
