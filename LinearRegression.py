ec8760200f1f28f46cb5cccf73722748bcf2cec2
Hitesh Sabnani <hiteshsabnani3@gmail.com>
2018-09-27 08:56:51 -0700

    added minibatch gradient descent

--- LinearRegression.py
+++ LinearRegression.py
@@ -5,7 +5,7 @@ import time
 class LinearRegression(BaseEstimator):
 
     def __init__(self, learning_rate=0.001, reg_strength=0, regularization="Ridge", max_iter=1000, cost_threshold=None,
-                 iteration_threshold=100, method="GD"):
+                 iteration_threshold=100, method="GD", minibatch_size=1):
         self.learning_rate = learning_rate
         self.reg_strength = reg_strength
         self.regularization = regularization
@@ -13,6 +13,7 @@ class LinearRegression(BaseEstimator):
         self.max_iter = max_iter
         self.iteration_threshold = iteration_threshold
         self.method = method
+        self.minibatch_size = minibatch_size
 
     def fit(self, X, y):
         """
@@ -61,6 +62,29 @@ class LinearRegression(BaseEstimator):
                 for i in range(0,num_examples):
                     theta = theta - self.learning_rate * ((h - y)*X)[i,] - (1/num_examples) * self.learning_rate * self.reg_strength * theta
                     bias = bias - self.learning_rate * (h - y)[i,]
+                    h = bias + np.dot(X,theta.T)
+                curr_cost = np.sqrt((1/(2*num_examples)) * sum((h - y)**2))
+
+                iter += 1
+                if iter % self.iteration_threshold == 0:
+                    cost = np.append(cost, [curr_cost])
+                    iterations = np.append(iterations, [iter])
+
+                if iter >= self.max_iter:
+                    break
+
+        elif self.method == "Minibatch":
+            while self.cost_threshold is None or (prev_cost - curr_cost) > self.cost_threshold:
+                # shuffle at each epoch
+                data_idx = [i for i in range(0,num_examples)]
+                np.random.shuffle(data_idx)
+                X = X[data_idx,]
+                y = y[data_idx,]
+                prev_cost = curr_cost
+                for i in range(0,num_examples,self.minibatch_size):
+                    theta = theta - self.learning_rate * (1/self.minibatch_size) * sum(((h - y)*X)[i:i+self.minibatch_size,]) - \
+                            (1/num_examples) * self.learning_rate * self.reg_strength * theta
+                    bias = bias - self.learning_rate * (1/self.minibatch_size) * sum((h - y)[i:i+self.minibatch_size,])
                     h = bias + np.dot(X,theta.T)
                 curr_cost = np.sqrt((1/(2*num_examples)) * sum((h - y)**2))
 
